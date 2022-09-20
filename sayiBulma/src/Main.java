7ed5e5139734155d8d2f174ba9dc15f9bc00376b
shardx97 <ahmetnur007@gmail.com>
2022-09-20 23:26:49 +0300

    innition

---
+++ sayiBulma/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] sayilar = new int [] {1,2,5,7,9,0};
+        int aranacak = 6;
+
+        boolean varMı = false;
+
+        for (int sayi : sayilar){
+            if (sayi == aranacak) {
+                varMı = true;
+                break;
+            }
+        }
+
+        if(varMı) {
+            System.out.println("Sayı mevcuttur");
+        }else {
+            System.out.println("Sayı mevcut değildir");
+        }
+    }
+}
